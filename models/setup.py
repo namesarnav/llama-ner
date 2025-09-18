5434ef18e2cc02ef4cb2792a8eea67eda169da7b
Arnav Verma <arnavverma522@gmail.com>
2025-09-18 01:18:27 -0500

    n/a

--- models/setup.py
+++ models/setup.py
@@ -1,10 +1,17 @@
 import yaml
-from config import *
 from transformers import (
     LlamaForTokenClassification,
     LlamaTokenizer
 )
 
-config = yaml.safe_load('config/config.yml')
+config = yaml.safe_load('../config/config.yml')
 
 MODEL = config['model_id']
+TOKENIZER = config['tokenizer_id']
+
+
+def load_base_model(MODEL, num_labels):
+    return LlamaForTokenClassification.from_pretrained(MODEL, num_labels=num_labels)
+
+def load_tokenizer(TOKENIZER):
+    tokenizer = LlamaTokenizer.from_pretrained(TOKENIZER)
